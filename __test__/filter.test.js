dccc6f0369fc370740ff62eadbc40cf182db06ad
Tal Kohavy <talkohavy@gmail.com>
2023-12-09 18:00:35 +0200

    test 2

--- __test__/filter.test.js
+++ __test__/filter.test.js
@@ -1,6 +1,6 @@
 import assert from 'assert/strict';
 import { describe, it } from 'node:test';
-import { Filterer } from '../lib/index.js';
+import { Filterer } from '../dist/index.js';
 import { data } from './constants.js';
 
 /** @typedef {import('../lib').Filter} Filter */
