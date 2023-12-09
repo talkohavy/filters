af77af646037d44543aec01f9dee58db8cd0fedd
Tal Kohavy <talkohavy@gmail.com>
2023-12-09 18:27:42 +0200

    test 5

--- tsup.config.js
+++ tsup.config.js
@@ -1,5 +1,6 @@
-import fs from 'fs';
 import { defineConfig } from 'tsup';
+
+// import fs from 'fs';
 
 // The options here is derived from CLI flags.
 export default defineConfig((_options) => ({
@@ -16,26 +17,26 @@ export default defineConfig((_options) => ({
   // minify: true, // <--- You can minify the output, resulting into lower bundle sizes.
   // target: , // <--- The value for target defaults to compilerOptions.target in your tsconfig.json, or node14 if unspecified. For more information check out esbuild's target option.
   // env: process.NODE_ENV,
-  async onSuccess() {
-    // Step 1: copy README file as-is to dist
-    const readStreamReadmeMd = fs.createReadStream('./README.md');
-    const writeStreamReadmeMd = fs.createWriteStream('./dist/README.md');
-    readStreamReadmeMd.pipe(writeStreamReadmeMd);
+  // async onSuccess() {
+  //   // Step 1: copy README file as-is to dist
+  //   const readStreamReadmeMd = fs.createReadStream('./README.md');
+  //   const writeStreamReadmeMd = fs.createWriteStream('./dist/README.md');
+  //   readStreamReadmeMd.pipe(writeStreamReadmeMd);
 
-    // Step 2: get the package.json file
-    const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+  //   // Step 2: get the package.json file
+  //   const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
 
-    // Step 3: Remove all scripts
-    delete packageJson.scripts;
+  //   // Step 3: Remove all scripts
+  //   delete packageJson.scripts;
 
-    // Step 4: Change from private to public
-    packageJson.private = false;
-    packageJson.publishConfig.access = 'public';
+  //   // Step 4: Change from private to public
+  //   packageJson.private = false;
+  //   packageJson.publishConfig.access = 'public';
 
-    // Step 5: create new package.json file in dist
-    fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson));
+  //   // Step 5: create new package.json file in dist
+  //   fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson));
 
-    console.log('DONE !!!');
-    // return () => { cleanupFunction() }
-  },
+  //   console.log('DONE !!!');
+  //   // return () => { cleanupFunction() }
+  // },
 }));
