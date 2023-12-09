a01316f5b4c7c2a97f23c33cf06ec12d65cdcb6f
Tal Kohavy <talkohavy@gmail.com>
2023-12-10 01:21:40 +0200

    it works but IDE doesn't autocpmplete

--- tsup.config.js
+++ tsup.config.js
@@ -6,15 +6,15 @@ export default defineConfig((_options) => ({
   entry: ['lib/index.js'],
   publicDir: './lib',
   outDir: 'dist', // <--- defaults to dist
-  bundle: false,
-  // minify: true, // <--- You can minify the output, resulting into lower bundle sizes.
-  // sourcemap: true, // <-- If you don't minify you don't need sourcemaps! This will emit a ./dist/index.js.map.
-  splitting: true, // <--- defaults to true. Code splitting currently only works with the esm output format.
+  bundle: false, // <--- defaults to true.
   format: ['esm'], // <-- If package.json type is set to module, the filenames are: [.js,.cjs], else: [.mjs, .js].
-  clean: true, // <--- Should clean output directory before each build?
+  splitting: false, // <--- defaults to true. Code splitting currently only works with the esm output format.
   treeshake: true, // <--- esbuild has tree shaking enabled by default, but sometimes it's not working very well, so tsup offers an additional option to let you use Rollup for tree shaking instead. This flag will enable Rollup for tree shaking.
+  clean: true, // <--- Should clean output directory before each build?
   // dts: true, // <--- Generate declaration file, meaning a index.d.ts.
-  // target: , // <--- The value for target defaults to compilerOptions.target in your tsconfig.json, or node14 if unspecified. For more information check out esbuild's target option.
+  // sourcemap: true, // <-- If you don't minify you don't need sourcemaps! This will emit a ./dist/index.js.map.
+  // minify: true, // <--- You can minify the output, resulting into lower bundle sizes.
+  target: 'esnext', // <--- The value for target defaults to compilerOptions.target in your tsconfig.json, or node14 if unspecified. For more information check out esbuild's target option.
   // env: process.NODE_ENV,
   async onSuccess() {
     // Step 1: copy README file as-is to dist
