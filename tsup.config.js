e3a13dda384c486a74b35fa863fda9c40c1aa517
Tal Kohavy <talkohavy@gmail.com>
2024-01-22 01:04:15 +0200

    updated tsupConfig

--- tsup.config.js
+++ tsup.config.js
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import { defineConfig } from 'tsup';
 
-// The options here is derived from CLI flags.
-export default defineConfig((_options) => ({
+const outDir = 'dist';
+
+// The `options` here is derived from CLI flags.
+const tsupConfig = defineConfig((_options) => ({
   entry: ['src/index.js'],
   publicDir: './src',
-  outDir: 'dist', // <--- defaults to dist
+  outDir, // <--- defaults to dist
   bundle: false, // <--- defaults to true.
   format: ['esm'], // <-- If package.json type is set to module, the filenames are: [.js,.cjs], else: [.mjs, .js].
   splitting: false, // <--- defaults to true. Code splitting currently only works with the esm output format.
@@ -17,25 +19,43 @@ export default defineConfig((_options) => ({
   target: 'esnext', // <--- The value for target defaults to compilerOptions.target in your tsconfig.json, or node14 if unspecified. For more information check out esbuild's target option.
   // env: process.NODE_ENV,
   async onSuccess() {
-    // Step 1: copy README file as-is to dist
-    const readStreamReadmeMd = fs.createReadStream('./README.md');
-    const writeStreamReadmeMd = fs.createWriteStream('./dist/README.md');
-    readStreamReadmeMd.pipe(writeStreamReadmeMd);
+    copyTheReadmeFile();
 
-    // Step 2: get the package.json file
-    const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+    copyTheNpmrcFile();
 
-    // Step 3: Remove all scripts
-    delete packageJson.scripts;
+    copyThePackageJsonFile();
 
-    // Step 4: Change from private to public
-    packageJson.private = false;
-    packageJson.publishConfig.access = 'public';
-
-    // Step 5: create new package.json file in dist
-    fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson));
-
-    console.log('DONE !!!');
-    // return () => { cleanupFunction() }
+    // Step 5: run the cleanup function
+    return () => {
+      // cleanupFunction()
+      console.log('DONE !!!');
+    };
   },
 }));
+
+function copyTheReadmeFile() {
+  const readStreamReadmeMd = fs.createReadStream('./README.md');
+  const writeStreamReadmeMd = fs.createWriteStream(`./${outDir}/README.md`);
+  readStreamReadmeMd.pipe(writeStreamReadmeMd);
+}
+
+function copyTheNpmrcFile() {
+  const readStreamNpmrc = fs.createReadStream('./.npmrc');
+  const writeStreamNpmrc = fs.createWriteStream(`./${outDir}/.npmrc`);
+  readStreamNpmrc.pipe(writeStreamNpmrc);
+}
+
+function copyThePackageJsonFile() {
+  const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+
+  // - Remove all scripts
+  delete packageJson.scripts;
+
+  // - Change from private to public
+  delete packageJson.private;
+  packageJson.publishConfig.access = 'public';
+
+  fs.writeFileSync(`./${outDir}/package.json`, JSON.stringify(packageJson));
+}
+
+export default tsupConfig;
