f8c3788df451694123065de0148dd0787d90d30b
Tal Kohavy <talkohavy@gmail.com>
2025-08-22 10:42:50 +0300

    created a better build job

--- build.config.mjs
+++ build.config.mjs
@@ -1,6 +1,5 @@
 import { execSync } from 'child_process';
 import fs, { cpSync } from 'fs';
-import os from 'os';
 import path from 'path';
 
 /**
@@ -8,72 +7,50 @@ import path from 'path';
  *   version: string,
  *   private?: string | boolean,
  *   main: string,
+ *   type: 'module' | 'commonjs'
  *   types: string,
  *   scripts?: Record<string, string>,
  *   publishConfig: {
  *     access: string
  *   },
+ *   devDependencies?: Record<string, string>,
  * }} PackageJson
  */
 
 const ROOT_PROJECT = process.cwd();
-
 const outDirName = 'dist';
+const COLORS = {
+  green: '[32m',
+  blue: '[34m',
+  stop: '[39m',
+};
 
 buildPackageConfig();
 
 async function buildPackageConfig() {
   cleanDistDirectory();
 
-  buildWithTsc();
+  build();
 
   copyStaticFiles();
 
-  manipulatePackageJsonFile(); // <--- must come AFTER copy of static files
+  manipulatePackageJsonFile();
 
-  console.log(`${os.EOL}[34mDONE !!![39m${os.EOL}`);
+  console.log('DONE !!!');
 }
 
 function cleanDistDirectory() {
-  console.log('[32m- Step 1:[39m clear the dist directory');
-  if (os.platform() === 'win32') {
-    execSync(`rd /s /q ${outDirName}`);
-  } else {
-    execSync(`rm -rf ${outDirName}`);
-  }
+  console.log(`${COLORS.green}- Step 1:${COLORS.stop} clear the ${outDirName} directory`);
+  execSync(`rm -rf ${outDirName}`);
 }
 
-function buildWithTsc() {
-  console.log('[32m- Step 2:[39m build the output dir');
-  execSync('tsc -p ./tsconfig.json');
-}
-
-function manipulatePackageJsonFile() {
-  console.log('[32m- Step 5:[39m copy & manipulate the package.json file');
-
-  const packageJsonPath = path.resolve(ROOT_PROJECT, outDirName, 'package.json');
-
-  // Step 1: get the original package.json file
-  /** @type {PackageJson} */
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
-
-  // Step 2: Remove all scripts
-  delete packageJson.scripts;
-  console.log('  • [34mdeleted[39m `scripts` key');
-
-  // Step 3: Change from private to public
-  delete packageJson.private;
-  packageJson.publishConfig.access = 'public';
-  console.log('  • [34mchanged[39m from private to public');
-  console.log('  • [34mchanged[39m publishConfig access to public');
-
-  // Step 4: create new package.json file in the output folder
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
-  console.log('  • [34mpackage.json[39m file written successfully!');
+function build() {
+  console.log(`${COLORS.green}- Step 2:${COLORS.stop} build`);
+  execSync('rollup --config'); // or the full command: rollup --config rollup.config.mjs
 }
 
 function copyStaticFiles() {
-  console.log('[32m- Step 3:[39m copy static files');
+  console.log(`${COLORS.green}- Step 3:${COLORS.stop} copy static files`);
 
   const filesToCopyArr = [
     { filename: 'package.json', sourceDirPath: [], destinationDirPath: [] },
@@ -96,4 +73,28 @@ function copyStaticFiles() {
       throw new Error('File MUST exists in order to PASS build process! cp operation failed...');
     }
   });
+}
+
+function manipulatePackageJsonFile() {
+  console.log(`${COLORS.green}- Step 4:${COLORS.stop} copy & manipulate the package.json file`);
+
+  const packageJsonPath = path.resolve(ROOT_PROJECT, outDirName, 'package.json');
+
+  // Step: get the original package.json file
+  /** @type {PackageJson} */
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+
+  packageJson.type = 'commonjs';
+  delete packageJson.private;
+  delete packageJson.scripts;
+  delete packageJson.devDependencies;
+  packageJson.publishConfig.access = 'public';
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
+
+  console.log(`  • ${COLORS.blue}changed${COLORS.stop} type module to commonjs`);
+  console.log(`  • ${COLORS.blue}changed${COLORS.stop} from private to public`);
+  console.log(`  • ${COLORS.blue}deleted${COLORS.stop} "scripts" key`);
+  console.log(`  • ${COLORS.blue}deleted${COLORS.stop} "devDependencies" key`);
+  console.log(`  • ${COLORS.blue}changed${COLORS.stop} publishConfig access to public`);
+  console.log(`  • ${COLORS.blue}package.json${COLORS.stop} file written successfully!`);
 }
