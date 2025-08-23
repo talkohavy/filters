a8a412b23ebc1f154be0f5ff145d85be677c6c11
Tal Kohavy <talkohavy@gmail.com>
2025-08-23 12:06:19 +0300

    no need for OPERATORS_REQUIRING_FUNCTION

--- src/FilterScheme/logic/constants.ts
+++ src/FilterScheme/logic/constants.ts
@@ -1,11 +1,6 @@
 import { type OperatorNames, Operators } from '../../Operators/operators';
 
 /**
- * Operators that require custom functions
- */
-export const OPERATORS_REQUIRING_FUNCTION = ['custom'];
-
-/**
  * List of all valid comparison operators
  */
 export const VALID_OPERATORS = Object.getOwnPropertyNames(Operators.prototype).filter(
